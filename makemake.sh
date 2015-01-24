dc339ccef20f4da464418b20d726eb00dd6171fb
Willoop <Will@resnet-097-139.ucdavis.edu>
2015-01-23 22:42:54 -0800

    ok

--- makemake.sh
+++ makemake.sh
@@ -6,17 +6,48 @@ echo "usage: makemake.sh executable_name"
 exit
 fi
 
-argu=""
 obejects=""
-
-echo $1 >> Makefile
-
+echo -n "$1 :" > Makefile
+name=$1
 shift
+constant="g++ -ansi -Wall -g -c"
+argu=""
+head=""
 
 for i in $@ ; do
     argu="$argu $i"
 done
 
 for i in $(ls *.cpp) ; do
+    objects="$objects ${i%%.*}"
+done
 
+for i in $objects ; do
+    echo -n " $i.o" >> Makefile
+done
+
+echo >> Makefile
+echo -n -e "\tg++ -ansi -Wall -g -o $name $argu " >> Makefile
+
+for i in $objects ; do
+   echo -n "$i.o " >> Makefile
+done
+
+echo >> Makefile
+echo >> Makefile
+
+for i in $objects ; do
+    echo -n "$i.o :" >> Makefile
+    echo -n " $i.cpp " >> Makefile
+    echo -n $(grep "^#include \".*\.h\"" "$i.cpp" | awk '{print $2}' | sed 's/"/ /g') >> Makefile
+    echo >> Makefile
+    echo -e "\t$constant $argu $i.cpp" >> Makefile
+    echo >> Makefile
+done
+
+echo "clean :" >> Makefile
+echo -n -e "\trm -f $name" >> Makefile
+
+for i in $objects ; do
+    echo -n " $i.o" >> Makefile
 done
